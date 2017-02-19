fae7bb0134aa5e2214b5b2ee97a2dab50822095e
Tamas Balint <balint.tamas87@gmail.com>
2017-02-19 22:15:20 +0000

    initialize form validation js and get input fields

---
+++ scripts/form-validate.js
@@ -0,0 +1,17 @@
+"use strict";
+
+(function () { // IIFE to avoid not to mess up the global namespace
+
+	//------------------------------------------------------
+	//-------INITIAL VALUES & NECCESSARY ASSIGNMENTS--------
+	//------------------------------------------------------
+	
+	var $brochureRequestFirstName = document.getElementById('first-name');
+	var $brochureRequestLastName = document.getElementById('last-name');
+	var $brochureRequestDOBDay = document.getElementById('dob-day');
+	var $brochureRequestDOBMonth = document.getElementById('dob-month');
+	var $brochureRequestYear = document.getElementById('dob-year');
+	var $brochureRequestEmail = document.getElementById('email');
+	var $brochureRequestEmailConfirm = document.getElementById('confirm-email');
+
+})();
